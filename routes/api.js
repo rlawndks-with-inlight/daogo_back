9fbd860a1681d43d167a1379c1fa99e8879c4336
rlawndks4 <rlawndks0423@naver.com>
2022-11-18 18:18:10 +0900

    asdf

--- routes/api.js
+++ routes/api.js
@@ -1583,8 +1583,6 @@ const addNotice = (req, res) => {
                 console.log(err)
                 return response(req, res, -200, "서버 에러 발생", [])
             } else {
-                sendAlarm("공지사항: " + title, "", "notice", result.insertId);
-                insertQuery("INSERT INTO alarm_log_table (title, note, item_table, item_pk, url) VALUES (?, ?, ?, ?, ?)", [title, "", "notice", result.insertId,`/post/notice/${result.insertId}`])
                 await db.query("UPDATE notice_table SET sort=? WHERE pk=?", [result?.insertId, result?.insertId], (err, resultup) => {
                     if (err) {
                         console.log(err)
@@ -1865,8 +1863,8 @@ const updateSetting = (req, res) => {
 }
 const updateStatus = (req, res) => {
     try {
-        const { table, pk, num } = req.body;
-        db.query(`UPDATE ${table}_table SET status=? WHERE pk=? `, [num, pk], (err, result) => {
+        const { table, pk, num, column } = req.body;
+        db.query(`UPDATE ${table}_table SET ${column}=? WHERE pk=? `, [num, pk], (err, result) => {
             if (err) {
                 console.log(err)
                 return response(req, res, -200, "서버 에러 발생", [])
